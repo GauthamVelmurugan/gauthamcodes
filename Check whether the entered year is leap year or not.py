6dfb3d8e3d53e6a6312f07067c7b8a006bd8bb71
GauthamVelmurugan <gauthamv2011@gmail.com>
2017-06-22 12:46:07 +0530

    Create Check whether the entered year is leap year or not.py

---
+++ Check whether the entered year is leap year or not.py
@@ -0,0 +1,12 @@
+def main():
+    a=int(raw_input("Enter a year to check whether it is a leap year or not"))
+    if(a%4==0):
+        print"The given year is a Leap year"
+    elif(a%4<>0):
+        print"The given year is not a leap year"
+    else:
+        exit
+
+
+if __name__ == '__main__':
+    main()
