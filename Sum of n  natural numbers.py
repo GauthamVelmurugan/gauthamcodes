37cf5163694ab523b6c6b83a5ea51dca14cb1b90
GauthamVelmurugan <gauthamv2011@gmail.com>
2017-06-22 14:48:49 +0530

    Add files via upload

---
+++ Sum of n  natural numbers.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      GauthamVelmurugan
+#
+# Created:     22-06-2017
+# Copyright:   (c) GauthamVelmurugan 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    a=int(raw_input("Enter a number"))
+    sum=0
+    for i in range(1,a+1):
+        sum=sum+i
+    print"Sum of n natural numbers is :",sum
+
+
+if __name__ == '__main__':
+    main()
